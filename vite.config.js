fc73d3706cb9d128016fe77b6c5851f65fc3cec9
elyes5992 <elyes.boughrara50@gmail.com>
2026-03-30 18:23:39 +0100

    m

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/Portfolio/",
   plugins: [react()],
+  build: {
+    modulePreload: false,
+    rollupOptions: {
+      external: []
+    }
+  }
 })
